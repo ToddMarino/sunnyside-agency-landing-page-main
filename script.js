544ce472709a490d0e40074c53c14ef9755684cf
ToddMarino <todd.j.marino@gmail.com>
2023-07-11 23:58:33 -0500

    refactor css and first media query

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-const menuIcon = document.getElementById('menu-icon')
-const navMenu = document.querySelector('.nav__menu')
+const menuIcon = document.querySelector('.menu-icon')
+const navMenu = document.querySelector('[data-nav]')
+const header = document.querySelector('header')
+const heroTitle = document.querySelector('.hero--title')
+const heroImage = document.querySelector('.hero--image')
 
-menuIcon.addEventListener('click', function () {
-    navMenu.classList.toggle('hidden')
+let isOpen = false
+
+menuIcon.addEventListener('click', () => {
+    if (isOpen === true) {
+        navMenu.classList.remove('menu-open')
+        heroTitle.classList.remove('hidden')
+        heroImage.classList.remove('hidden')
+        isOpen = false
+    } else {
+        navMenu.classList.add('menu-open')
+        heroTitle.classList.add('hidden')
+        heroImage.classList.add('hidden')
+        isOpen = true
+}
+
+
 })
